fb6f81f6d2c815414ff3674c4de9dfe92fa91e84
喻林 <yulin_yl@126.com>
2019-12-26 18:00:00 +0800

    acronym

--- exercises/acronym/lib/acronym.ex
+++ exercises/acronym/lib/acronym.ex
@@ -5,5 +5,10 @@ defmodule Acronym do
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
+    Regex.scan(~r/(?:[A-Z]+|[a-z])[a-z']*/, string)
+    |> List.flatten()
+    |> Enum.map(&String.first/1)
+    |> Enum.map(&String.upcase/1)
+    |> Enum.join()
   end
 end
